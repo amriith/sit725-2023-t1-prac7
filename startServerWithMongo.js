d19f787c90e7485e1130be511f1b12fca4645f6d
amriith <83343585+amriith@users.noreply.github.com>
2024-01-09 23:11:42 +1100

    done

--- startServerWithMongo.js
+++ startServerWithMongo.js
@@ -5,12 +5,26 @@ let port = process.env.port || 3000;
 require('./Dbconnections');
 let router = require('./router/router');
 let collection;
+const { Socket } = require('socket.io');
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/', router);
 
+io.on('connection', (socket) => {
+    console.log('New User connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    setInterval(() => {
+        socket.emit('number', parseInt(Math.random() * 10)
+        )
+    },1000)
+})
 
 app.listen(port, ()=>{
     console.log('express server started');  
